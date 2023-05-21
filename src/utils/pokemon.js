35f2831614d104746d198cd49744e697b77513c5
firesign2023 <firesign2023@gmail.com>
2023-05-21 22:28:17 +0900

    ポケモンAPIで取得

---
+++ src/utils/pokemon.js
@@ -0,0 +1,9 @@
+
+
+export function getAllPokemon(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => resolve(data));
+    });
+}
